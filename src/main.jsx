3c551e1470e2eb0320afb8ede3c3b066e9043df6
Malejo <alejoramirez6@yahoo.com>
2023-04-18 20:39:06 -0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,14 @@ import ReactDOM from 'react-dom/client';
 //import { Layout } from './05-uselayoutEffect/Layout';
 //import { MemoHook } from './06-memos/MemoHook';
 //import { CallBackHook } from './06-memos/CallBackHook';
-import { Padre } from './07-tarea-memo/Padre';
+//import { Padre } from './07-tarea-memo/Padre';
+//import './08-useReducer/intro-reducer';
+import { TodoApp } from './08-useReducer/TodoApp';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Padre />
+    <TodoApp />
  // </React.StrictMode>,
 )
